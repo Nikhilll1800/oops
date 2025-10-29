d528b32288fbee5c98f4afde42b9c9ccba828216
Nikhil Soni <nikhil2000soni@gmail.com>
2025-10-30 04:03:17 +0530

    the correct way to use O of solid in designing

--- sOlid.cpp
+++ sOlid.cpp
@@ -1,7 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Rectangle{
+
+
+//calulate area logic should be inside each shape so that each shape should know that how thier area is being calculated and we dont have to repeat the logic each and everytime we add a new shape; 
+
+
+class Shape{
+    public:
+    virtual double calculateArea() const=0;
+    virtual ~Shape(){} 
+};
+
+class Rectangle: public Shape{
     public:
     double length;
     double breadth;
@@ -9,30 +20,45 @@ class Rectangle{
             this->length=length;
             this->breadth=breadth;
         }
+        // override
+        double calculateArea() const override{
+            return this->length *this->breadth;
+        }
 };
-class Circle{
+class Circle:public Shape{
     public:
     double radius;
         Circle(double radius){
             this->radius=radius;
         }
+         double calculateArea() const override{
+            return M_PI* this->radius* this->radius;
+        }
 };
 
-class areaCalculator{
-    public:
-    double calulateArea(const Rectangle& rect){
-        // Rectangle rect=(Rectangle)shape;
-        return rect.length * rect.breadth;
-    }
-     double calulateArea(const Circle &circle){
-        return M_PI*circle.radius * circle.radius;
-    }
-};
+// class areaCalculator{
+//     public:
+//     double calulateArea(const Rectangle& rect){
+//         // Rectangle rect=(Rectangle)shape;
+//         return rect.length * rect.breadth;
+//     }
+//      double calulateArea(const Circle &circle){
+//         return M_PI*circle.radius * circle.radius;
+//     }
+// };
 
 int main(){
     Rectangle rect(4,5);
     Circle cir(5);
-    areaCalculator area;
-    cout<<"the area of rect is "<< area.calulateArea(rect)<<endl;
-    cout<<"the area of circle is "<< area.calulateArea(cir);
+    // Use polymorphism
+    Shape* shapes[] = { &rect, &cir };
+
+    for (auto shape : shapes) {
+        cout << "Area: " << shape->calculateArea() << endl;
+    }
+
+    return 0;
+    // areaCalculator area;
+    // cout<<"the area of rect is "<< rect.calulateArea(rect)<<endl;
+    // cout<<"the area of circle is "<< area.calulateArea(cir);
 }
