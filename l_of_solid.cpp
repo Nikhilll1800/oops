2f58189ec894def9cd73d27cb95af7f8fdfc0fe8
Nikhil Soni <nikhil2000soni@gmail.com>
2025-10-31 06:34:59 +0530

    the l of solid

--- l_of_solid.cpp
+++ l_of_solid.cpp
@@ -1,6 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-class 
+// interface
+class Shape{
+    public:
+    virtual int getArea() const =0;
+    virtual ~Shape()=default;
+};
+class Rectangle:public Shape{
+    public:
+    int width;
+    int height;
+
+    Rectangle(int height,int width){
+        this->height=height;
+        this->width=width;
+    }
+    int getArea()const override{
+        return height*width;
+    } 
+
+};
+class Square:public Shape{
+    public:
+    int side;
+
+    Square(int side){
+        this->side=side;
+        // this->width=width;
+    }
+    int getArea()const override{
+        return side*side;
+    } 
+
+};
 int main(){
+    Shape* rect=new Rectangle(4,5);
+    Shape* square=new Square(5);
+    cout<<rect->getArea();
+    cout<<endl;
+    cout<<square->getArea();
+    delete rect;
+    delete square;
     return 0;
 }
